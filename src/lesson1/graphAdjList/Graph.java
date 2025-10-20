a8882a42d5fae9c255901bcb3e814606cf326296
DenisStepanidenko <stepanidenko.den@mail.ru>
2025-10-21 02:48:43 +0300

    hometask1

---
+++ src/lesson1/graphAdjList/Graph.java
@@ -0,0 +1,97 @@
+package lesson1.graphAdjList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Реализация графа через список смежностей
+ */
+
+public class Graph {
+
+    private List<Edge>[] graph;
+
+    /**
+     * Конструктор
+     *
+     * @param n кол-во вершин графа
+     */
+    public Graph(int n) {
+
+        graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+    }
+
+    public void addEdge(int v, int w, double capacity, double loss) {
+
+        graph[v].add(new Edge(v, w, capacity, loss));
+        graph[w].add(new Edge(w, v, capacity, loss));
+
+    }
+
+
+    static class Edge {
+
+        private int from;
+        private int to;
+
+        private double capacity;
+        private double loss;
+
+        public Edge(int from, int to, double capacity, double loss) {
+            this.from = from;
+            this.to = to;
+            this.capacity = capacity;
+            this.loss = loss;
+
+        }
+
+        public int getFrom() {
+            return from;
+        }
+
+        public double getLoss() {
+            return loss;
+        }
+
+
+        public double getCapacity() {
+            return capacity;
+        }
+
+
+        public int getTo() {
+            return to;
+        }
+
+    }
+
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < graph.length; i++) {
+
+            sb.append("-------------");
+            sb.append("Вершина ").append(i).append("\n");
+            List<Edge> edges = graph[i];
+
+            sb.append("Сколько соседних вершин: ").append(edges.size()).append("\n");
+            sb.append("Список соседних вершин ").append("\n");
+            for (Edge edge : edges) {
+                sb.append("Вершина: ").append(edge.to).append(",capacity: ").append(edge.capacity).append(",loss: ").append(edge.loss).append("\n");
+            }
+
+            sb.append("\n");
+
+
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/lesson1/graphMatrix/Graph.java
@@ -0,0 +1,73 @@
+package lesson1.graphMatrix;
+
+import java.util.Arrays;
+
+/**
+ * Представление графа в виде массива
+ */
+public class Graph {
+
+    private double[][] capacity;
+    private double[][] loss;
+
+    private int[][] matrix;
+
+    /**
+     * Конструктор
+     *
+     * @param n кол-во вершин графа
+     */
+    public Graph(int n) {
+
+        capacity = new double[n][n];
+        loss = new double[n][n];
+        matrix = new int[n][n];
+
+    }
+
+    public void addEdge(int v, int w, double capacity, double loss) {
+
+        this.capacity[v][w] = capacity;
+        this.loss[v][w] = loss;
+
+        matrix[v][w] = 1;
+        matrix[w][v] = 1;
+
+    }
+
+
+    public double[][] getCapacity() {
+        return capacity;
+    }
+
+    public double[][] getLoss() {
+        return loss;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            sb.append("-------------");
+            sb.append("Вершина ").append(i);
+            sb.append("Соседние вершины ");
+
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 1) {
+                    sb.append("Вершина: ").append(j).append(",capacity: ").append(capacity[i][j]).append(",loss: ").append(loss[i][j]).append("\n");
+                }
+            }
+
+
+        }
+
+        return sb.toString();
+    }
+}
